4e18b3e34f6b802d22ae11cd1fd1ff2dbdcdb905
Apostolos Modas <modas@lts4srv2.epfl.ch>
2018-10-31 20:54:04 +0100

    SparseFool algorithm

---
+++ sparsefool.py
@@ -0,0 +1,33 @@
+import torch as torch
+import copy
+from linear_solver import linear_solver
+from torch.autograd import Variable
+from utils import clip_image_values
+from deepfool import deepfool
+
+
+def sparsefool(x_0, net, lb, ub, lambda_fac=6., max_iter=20, epsilon=0.02):
+
+    pred_label = torch.argmax(net.forward(Variable(x_0, requires_grad=True)).data).item()
+
+    x_i = copy.deepcopy(x_0)
+    fool_im = copy.deepcopy(x_i)
+
+    fool_label = pred_label
+    loops = 0
+
+    while fool_label == pred_label and loops < max_iter:
+
+        normal, x_adv = deepfool(x_i, net, lambda_fac)
+
+        x_i = linear_solver(x_i, normal, x_adv, lb, ub)
+
+        fool_im = x_0 + (1 + epsilon) * (x_i - x_0)
+        fool_im = clip_image_values(fool_im, lb, ub)
+        fool_label = torch.argmax(net.forward(Variable(fool_im, requires_grad=True)).data).item()
+
+        loops += 1
+
+    r = fool_im - x_0
+
+    return fool_im, r, pred_label, fool_label, loops
