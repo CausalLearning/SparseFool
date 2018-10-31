5576e11c6cefb3f64a8b6803b00b4a19a3182377
Apostolos Modas <modas@lts4srv2.epfl.ch>
2018-10-31 20:52:28 +0100

    DeepFool algorithm

---
+++ deepfool.py
@@ -0,0 +1,70 @@
+import numpy as np
+import torch
+import copy
+from torch.autograd.gradcheck import zero_gradients
+from torch.autograd import Variable
+
+
+def deepfool(im, net, lambda_fac=6., num_classes=10, overshoot=0.02, max_iter=50):
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    image = copy.deepcopy(im)
+    input_shape = image.size()
+
+    f_image = net.forward(Variable(image, requires_grad=True)).data.cpu().numpy().flatten()
+    I = (np.array(f_image)).flatten().argsort()[::-1]
+    I = I[0:num_classes]
+    label = I[0]
+
+    pert_image = copy.deepcopy(image)
+    r_tot = torch.zeros(input_shape).to(device)
+
+    k_i = label
+    loop_i = 0
+
+    while k_i == label and loop_i < max_iter:
+
+        x = Variable(pert_image, requires_grad=True)
+        fs = net.forward(x)
+
+        pert = torch.Tensor([np.inf])[0].to(device)
+        w = torch.zeros(input_shape).to(device)
+
+        fs[0, I[0]].backward(retain_graph=True)
+        grad_orig = copy.deepcopy(x.grad.data)
+
+        for k in range(1, num_classes):
+            zero_gradients(x)
+
+            fs[0, I[k]].backward(retain_graph=True)
+            cur_grad = copy.deepcopy(x.grad.data)
+
+            w_k = cur_grad - grad_orig
+            f_k = (fs[0, I[k]] - fs[0, I[0]]).data
+
+            pert_k = torch.abs(f_k) / w_k.norm()
+
+            if pert_k < pert:
+                pert = pert_k + 0.
+                w = w_k + 0.
+
+        r_i = torch.clamp(pert, min=1e-4) * w / w.norm()
+
+        r_tot = r_tot + r_i
+        pert_image = pert_image + r_i
+
+        check_fool = image + (1 + overshoot) * r_tot
+        k_i = torch.argmax(net.forward(Variable(check_fool, requires_grad=True)).data).item()
+
+        loop_i += 1
+
+    x = Variable(pert_image, requires_grad=True)
+    fs = net.forward(x)
+    (fs[0, k_i] - fs[0, label]).backward(retain_graph=True)
+    grad = x.grad.data
+
+    r_tot = lambda_fac * r_tot
+    pert_image = image + r_tot
+
+    return grad, pert_image
