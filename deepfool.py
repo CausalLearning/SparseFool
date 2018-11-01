ae0ece1b2489fa285dedbc250fab60ae2af5d5c3
Apostolos Modas <modas@lts4srv2.epfl.ch>
2018-11-01 12:17:51 +0100

    lambda

--- deepfool.py
+++ deepfool.py
@@ -5,7 +5,7 @@ from torch.autograd.gradcheck import zero_gradients
 from torch.autograd import Variable
 
 
-def deepfool(im, net, lambda_fac=6., num_classes=10, overshoot=0.02, max_iter=50):
+def deepfool(im, net, lambda_fac=3., num_classes=10, overshoot=0.02, max_iter=50):
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
